be8625a0e312ed1fa2c8397a4ddfea2a7569c871
dumisanimagagula <dumsaneemmanuel98@gmail.com>
2022-12-31 07:44:24 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the numbers 1 - 100
+ * multiplies of 3 print Fizz
+ * multiplies of 5 print Buzz
+ * multiplies of 3 & 5 print FizzBuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s", fb);
+		else if (i % 3 == 0)
+			printf("%s", f);
+		else if (i % 5 == 0)
+			printf("%s", b);
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
